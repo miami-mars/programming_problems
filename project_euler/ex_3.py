5fe3cb66d25a405e77d8e578a300d2954a0bc8d9
JuniorJorge <me@juniorjorge.com>
2018-01-02 09:30:45 -0500

    Update ex_3.py
    Instead of checking every number from 1 to half, I check every number
    from 1 to the square root of the number supplied. This cuts down the
    amount of computation significantly.

--- project_euler/ex_3.py
+++ project_euler/ex_3.py
@@ -1,16 +1,27 @@
+from math import sqrt
+
 def is_prime(number):
     for i in range(2, number):
         if number % i == 0:
             return False
     return True
 
-def prime_factors(number):
-    fact_list = [1]
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            fact_list.append(i)
+def prime_factors(fact_list):
     prime_list = []
     for x in fact_list:
         if is_prime(x):
             prime_list.append(x)
     return prime_list 
+
+def factors(number):
+    fact_list = [1]
+    for i in range(2, int(sqrt(number))):
+        if number % i == 0:
+            fact_list.append(i)
+    fact_list.append(number)
+    return fact_list
+
+if __name__ == '__main__':
+    factor_list = factors(600851475143)
+    prime_list = prime_factors(factor_list)
+    print(prime_list)
